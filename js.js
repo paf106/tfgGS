fe4c7305f048eac0fbb4164ebb1794936896ad2d
paf106 <pabloavila106@gmail.com>
2020-02-01 02:42:10 +0100

    Javascript para validar la contraseña HECHO

--- js.js
+++ js.js
@@ -35,22 +35,77 @@ function goToAdminPage(){
 
 
 // JAVASCRIPT REGISTRO
+function strengthCapitalPassword() {
+	var pass = document.getElementById('passwordSign-up').value;
+	var tieneMayus = false;
+
+	 var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
+					"P","Q","R","S","T","U","V","W","X","Y","Z"];
+	for (var i = 0; i < letters.length; i++){
+		if (!pass.includes(letters[i])){
+			//alert('Your password must have at least one capital letter');
+			
+			
+		}else{
+			alert('Your password has one capital letter');
+			tieneMayus = true;
+			break;
+		}
+	}
+    if(tieneMayus == true){
+    return true;
+    }else{
+    alert('Your password must have at least one capital letter');
+    }
+}
+function strengthNumberPassword(){
+	var pass = document.getElementById('passwordSign-up').value;
+	var tieneNumero = false;
+
+	var numbers = [1,2,3,4,5,6,7,8,9,0];
+	for (var i = 0; i < numbers.length; i++){
+		if (!pass.includes(numbers[i])){
+			//alert('Your password must have at least one number');
+			
+			
+		}else{
+			alert('Your password has one number');
+			tieneNumero = true;
+			break;
+		}
+	}
+    if(tieneNumero == true){
+    return true;
+    }else{
+    alert('Your password must have at least one number');
+    }
+}
+function strengthLengthPassword(){
+	var pass = document.getElementById('passwordSign-up').value;
+	if (pass.length<8){
+		alert('Your password must contain at least 8 characters');
+	}else{
+		alert('Your password contains 8 characters');
+		return true;
+
+	}
+}
 function checkPassword(){
 	var pass = document.getElementById('passwordSign-up').value;
 	var repeatPass = document.getElementById('repeatPasswordSign-up').value;
+
+
 	if (pass != repeatPass){		
 		alert('Passwords are not the same');
 		document.getElementById('passwordSign-up').value = "";
 		document.getElementById('repeatPasswordSign-up').value = "";
-		document.getElementById('passwordSign-up').style.borderColor = "red";
-		document.getElementById('repeatPasswordSign-up').style.borderColor = "red";
+		document.getElementById('passwordSign-up').focus();
 		return false;
 	}else{
-		//document.getElementById('passwordSign-up').style.borderStyle = "solid";
-		//document.getElementById('repeatPasswordSign-up').style.borderStyle = "solid";
-		document.getElementById('passwordSign-up').style.borderColor = "lightgrey";
-		document.getElementById('repeatPasswordSign-up').style.borderColor = "lightgrey";
-		return true;
+		strengthNumberPassword();
+		strengthCapitalPassword();
+		strengthLengthPassword();
+
 	}
 }
 
