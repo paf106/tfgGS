11ab4cb88ce596250dd3cad59d973e2ff27f2ebc
paf106 <pabloavila106@gmail.com>
2020-02-06 00:16:49 +0100

    'strengthLengthPassword()' y 'checkUser()' comprobado que funciona bien

--- js.js
+++ js.js
@@ -37,27 +37,18 @@ function goToAdminPage(){
 // JAVASCRIPT REGISTRO
 function strengthCapitalPassword() {
 	var pass = document.getElementById('passwordSign-up').value;
-	var tieneMayus = false;
 
-	var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
-					"P","Q","R","S","T","U","V","W","X","Y","Z"];
+	var letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+					'P','Q','R','S','T','U','V','W','X','Y','Z'];
 	for (var i = 0; i < letters.length; i++){
-		if (!pass.includes(letters[i])){
-			//alert('Your password must have at least one capital letter');
-			//return false;
-		}else{
-			//alert('Your password has one capital letter');
-			tieneMayus = true;
+		if (pass.includes(letters[i])){
+			//alert('Good password');
 			return true;
-			//break;
+		}else{
+        alert('Your password must have at least one capital letter');
+			return false;
 		}
 	}
-    if(tieneMayus == true){
-    return true;
-    }else{
-    alert('Your password must have at least one capital letter');
-    return false;
-    }
 }
 function strengthNumberPassword(){
 	var pass = document.getElementById('passwordSign-up').value;
@@ -72,7 +63,7 @@ function strengthNumberPassword(){
 		}else{
 			//alert('Your password has one number');
 			tieneNumero = true;
-			return true;
+			//return true;
 			//break;
 		}
 	}
@@ -83,7 +74,7 @@ function strengthNumberPassword(){
     return false;
     }
 }
-function strengthLengthPassword(){
+function strengthLengthPassword(){ //FUNCIONA BIEN
 	var pass = document.getElementById('passwordSign-up').value;
 	if (pass.length<8){
 		alert('Your password must contain at least 8 characters');
@@ -92,9 +83,8 @@ function strengthLengthPassword(){
 		//alert('Your password contains 8 characters');
 		return true;
 	}
-	//alert("strengthLengthPassword()" + strengthLengthPassword());
 }
-function checkUser(){
+function checkUser(){ //FUNCIONA BIEN
 	var user = document.getElementById('userSign-up').value;
 	
 	if (user.length<5 || user.length>15){
@@ -139,67 +129,6 @@ function checkPassword(){
 		  alert ("Password cannot contain blank spaces");
 		  return false;
 		}
-}
-
-function progressBarUser(){
-	var userSign_up = document.getElementById('userSign-up').value;
-	var barra = document.getElementById('bar').value;
-
-	if (userSign_up != ""){
-		document.getElementById('bar').value = barra+1;
-	}else{
-		document.getElementById('bar').value = barra-1;
-	}
-}
-function progressBarPassword(){
-	var passwordSign_up = document.getElementById('passwordSign-up').value;
-	var barra = document.getElementById('bar').value;
-
-	if (passwordSign_up != ""){
-		document.getElementById('bar').value = barra+1;
-	}else{
-		document.getElementById('bar').value = barra-1;
-	}
-}
-function progressBarRepeatPassword(){
-	var repeatPasswordSign_up = document.getElementById('repeatPasswordSign-up').value;
-	var barra = document.getElementById('bar').value;
-
-	if (repeatPasswordSign_up != ""){
-		document.getElementById('bar').value = barra+1;
-	}else{
-		document.getElementById('bar').value = barra-1;
-	}
-}
-function progressBarMail(){
-	var mailSign_up = document.getElementById('mailSign-up').value;
-	var barra = document.getElementById('bar').value;
-
-	if (mailSign_up != ""){
-		document.getElementById('bar').value = barra+1;
-	}else{
-		document.getElementById('bar').value = barra-1;
-	}
-}
-function progressBarName(){
-	var nameSign_up = document.getElementById('nameSign-up').value;
-	var barra = document.getElementById('bar').value;
-
-	if (nameSign_up != ""){
-		document.getElementById('bar').value = barra+1;
-	}else{
-		document.getElementById('bar').value = barra-1;
-	}
-}
-function progressBarSurname(){
-	var surnameSign_up = document.getElementById('surnameSign-up').value;
-	var barra = document.getElementById('bar').value;
-
-	if (surnameSign_up != ""){
-		document.getElementById('bar').value = barra+1;
-	}else{
-		document.getElementById('bar').value = barra-1;
-	}
 }
 // FIN JAVASCRIPT REGISTRO
 
