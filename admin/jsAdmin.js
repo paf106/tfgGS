f63b8c68705573023d9b6323b2d149638d3e518a
paf106 <pabloavila106@gmail.com>
2020-03-17 19:59:50 +0100

    code renovation

--- admin/jsAdmin.js
+++ admin/jsAdmin.js
@@ -1,50 +1,56 @@
 /* JAVASCRIPT ADMINISTRACION USUARIO */
-function delUserConfirmation(){
+function openAlert(divAMostrar){
 	/* Pone header, section y footer en fondo blur */
 	document.getElementById("contenedorConfirmacion").style.display = "flex";
 	document.getElementById("contenedorConfirmacion").style.zIndex = "1";
-	document.getElementById("contenedorDelUser").style.zIndex = "1";
-	document.getElementById("contenedorDelUser").style.display = "block";
-
-	document.querySelector("header").style.filter = "blur(2px)";
-	document.querySelector("section").style.filter = "blur(2px)";
-	document.querySelector("footer").style.filter = "blur(2px)";
-
-	
-}
-function closeAlertDelUser(){
-	/* Quita header, section y footer del fondo blur */
-	
-	document.querySelector("header").style.filter = "none";
-	document.querySelector("section").style.filter = "none";
-	document.querySelector("footer").style.filter = "none";
-	document.getElementById("contenedorConfirmacion").style.zIndex = "auto";
-	document.getElementById("contenedorDelUser").style.zIndex = "auto";
-
-	document.getElementById("contenedorConfirmacion").style.display = "none";
-	document.getElementById("contenedorDelUser").style.display = "none";
-	
-}
-function editUserConfirmation(){
-	/* Pone header, section y footer en fondo blur */
-	document.getElementById("contenedorConfirmacion").style.display = "flex";
-	document.getElementById("contenedorConfirmacion").style.zIndex = "1";
-	document.getElementById("contenedorEditUser").style.zIndex = "1";
-	document.getElementById("contenedorEditUser").style.display = "block";
+	document.getElementById(divAMostrar).style.zIndex = "1";
+	document.getElementById(divAMostrar).style.display = "block";
 
 	document.querySelector("header").style.filter = "blur(2px)";
 	document.querySelector("section").style.filter = "blur(2px)";
 	document.querySelector("footer").style.filter = "blur(2px)";
 }
-function cancelAlertEditUser(){
+function closeAlert(divACerrar){
 	/* Quita header, section y footer del fondo blur */
 	document.querySelector("header").style.filter = "none";
 	document.querySelector("section").style.filter = "none";
 	document.querySelector("footer").style.filter = "none";
+	/* */
 	document.getElementById("contenedorConfirmacion").style.zIndex = "auto";
-	document.getElementById("contenedorEditUser").style.zIndex = "auto";
+	document.getElementById(divACerrar).style.zIndex = "auto";
 
 	document.getElementById("contenedorConfirmacion").style.display = "none";
-	document.getElementById("contenedorEditUser").style.display = "none";
+	document.getElementById(divACerrar).style.display = "none";
+}
+function openEditPassAlert(){
+		/* Quita header, section y footer del fondo blur */
+	document.querySelector("header").style.filter = "none";
+	document.querySelector("section").style.filter = "none";
+	document.querySelector("footer").style.filter = "none";
+	/* */
+	document.getElementById("contenedorConfirmacion").style.zIndex = "auto";
+	document.getElementById("contenedorSelectUser").style.zIndex = "auto";
+
+	document.getElementById("contenedorConfirmacion").style.display = "none";
+	document.getElementById("contenedorSelectUser").style.display = "none";
+	
+	//Capturamos el usuario al cual le queremos cambiar la contraseña
+	var userChangePass = document.getElementsByName('userList');
+
+		/* Pone header, section y footer en fondo blur */
+	document.getElementById("contenedorConfirmacion").style.display = "flex";
+	document.getElementById("contenedorConfirmacion").style.zIndex = "1";
+	document.getElementById("contenedorEditPass").style.zIndex = "1";
+	document.getElementById("contenedorEditPass").style.display = "block";
+
+	document.querySelector("header").style.filter = "blur(2px)";
+	document.querySelector("section").style.filter = "blur(2px)";
+	document.querySelector("footer").style.filter = "blur(2px)";
+
+	return userChangePass;
+}
+function checkPass(campo){
+	var pass = document.getElementById(campo).value;
+	return pass;
 }
 /* FIN JAVASCRIPT ADMINISTRACION USUARIO */
